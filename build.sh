7ec847c9089eb06c99c9a0a5145fceb28a121a1f
Mobyus <theonlymoby@protonmail.com>
2022-07-09 22:17:09 +0300

    build.sh checks for raylib ;)

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 #Works only on GNU Linux 
 #Requires raylib installation
 set -e 
-gcc main.c utils.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o SortVisual
-./SortVisual
+
+[ -z `ldconfig -p | grep libraylib` ] && echo "Please, install raylib first." || gcc main.c utils.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o SortVisual && ./SortVisual
